5dbdb091150c887e5c0e24b9f20a3b0037728e38
AAYUSH116 <82816625+AAYUSH116@users.noreply.github.com>
2021-06-22 12:32:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX="";
+rightWristX="";
+difference1="";
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -10,13 +14,23 @@ function setup(){
 
 function draw(){
     background('#ffcccc');
+    text('Aayush',difference1);
+    fill('#ffcccc');
+    
 }
 
 function modelLoaded(){
     console.log('POSE NET IS INTIALIZED');
 }
 
+
+
+
 function gotPose(results){
 if(results.length>0){
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.y;
+    difference1= floor(rightWristX-leftWristX);
+    console.log("leftWristX"+leftWristX+"rightWristX"+rightWristX+"difference1"+difference1);
 }
 }
